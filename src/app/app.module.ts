8cd7526cca68e7dd3a0f3ba9ff7d92e641fd2a91
gmdmgithub <gmika@interia.pl>
2018-10-18 21:28:06 +0200

    reactive form validation - init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/signup-form/signup-form.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import {FormGroup, FormControl, Validators} from '@angular/forms';
+import { ReturnStatement } from '@angular/compiler';
+
+@Component({
+  selector: 'signup-form',
+  templateUrl: './signup-form.component.html',
+  styleUrls: ['./signup-form.component.css']
+})
+export class SignupFormComponent {
+  form = new FormGroup({
+    'username':new FormControl('',[
+      Validators.required,
+      Validators.minLength(3)
+    ]),
+    'passward':new FormControl('',Validators.required)
+  });
+
+  get username(){
+    return this.form.get('username');
+  }
+
+}
