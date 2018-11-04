8f6a4bfbfd552035c6126713a2325487d53b7b66
gmdmgithub <gmika@interia.pl>
2018-11-04 13:04:18 +0100

    ammends

--- src/app/github-followers/github-followers.component.ts
+++ src/app/github-followers/github-followers.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { GithubFollowersService } from '../github-followers.service';
 import { ActivatedRoute } from '@angular/router';
 
+// import { Observable } from 'rxjs/Observable';
+import {Observable} from 'rxjs/Rx'; //angular 6
+//import 'rxjs/add/observable/combineLatest'; //factory method (static)
+import { combineLatest } from 'rxjs';//angular 6
+
 @Component({
   selector: 'github-followers',
   templateUrl: './github-followers.component.html',
@@ -17,19 +22,30 @@ export class GithubFollowersComponent implements OnInit {
 
   ngOnInit() {
     
-    this.route.paramMap
-      .subscribe(params =>{
-      console.log(params);
+     Observable.combineLatest([
+      this.route.paramMap,
+      this.route.queryParamMap
+    ]).subscribe( combined => {      
+      let id =  combined[0].get('id');
+      let page =  combined[1].get('page');
+      console.log(id, page);
+      this.service.getAll().subscribe(followers =>this.followers = followers);
       
     });
     
-    this.route.queryParamMap
-      .subscribe(qparams =>{
-        console.log(qparams);
+    // this.route.paramMap
+    //   .subscribe(params =>{
+    //   console.log(params);
+      
+    // });
+    
+    // this.route.queryParamMap
+    //   .subscribe(qparams =>{
+    //     console.log(qparams);
 
-    });
+    // });
 
-    this.service.getAll().subscribe(followers =>this.followers = followers);
+    // this.service.getAll().subscribe(followers =>this.followers = followers);
   }
 
 }
--- src/app/github-profile/github-profile.component.ts
+++ src/app/github-profile/github-profile.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-github-profile',
@@ -8,13 +8,22 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class GithubProfileComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, //for parameters
+              private router: Router //for programatic routers
+              ) { }
 
   ngOnInit() {
     this.route.paramMap
     .subscribe(params =>{
       console.log(+params.get('id'));
       
+    })
+  }
+
+  submit(){
+    this.router.navigate(['/followers'],
+    {
+      queryParams: {page:1, order:"oldest"}
     })
   }
 
